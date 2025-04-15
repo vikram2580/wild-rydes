b7962e360c0c1e678c368b0e8ca27fedbb3a2285
vikram2580 <singh.vikramsingh06@gmail.com>
2025-04-15 12:22:24 -0400

    new files

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-# wildrydes
-
-A rebuild of the serverless website WildRydes Workshop in Vue.js with AWS Amplify CLI.
+# wildrydes-site
---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+  cognito: {
+    userPoolId: "us-east-1_P3KuR4DgH", // e.g. us-east-2_uXboG5pAb
+    userPoolClientId: "6rqjneuthaos01e0hgjhis10kt", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+    region: "us-east-1", // e.g. us-east-2
+  },
+  api: {
+    invokeUrl: "https://0l8fs7uz1b.execute-api.us-east-1.amazonaws.com/prod", // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+  },
+};
